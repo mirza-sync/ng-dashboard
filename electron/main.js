54f7228b289d0b99e6136d7cd9a02c003e56b73c
mirza-sync <mirzasyahmi123@gmail.com>
2026-06-19 16:33:42 +0800

    Setup electron

---
+++ electron/main.js
@@ -0,0 +1,28 @@
+// main.js
+const { app, BrowserWindow } = require("electron");
+const path = require("path");
+
+let mainWindow;
+
+function createWindow() {
+  mainWindow = new BrowserWindow({
+    width: 1280,
+    height: 800,
+    webPreferences: {
+      contextIsolation: true,
+      nodeIntegration: false,
+    },
+  });
+
+  mainWindow.loadFile(path.join(__dirname, "../dist/ng-dashboard/index.html"));
+
+  mainWindow.on("closed", function () {
+    mainWindow = null;
+  });
+}
+
+app.whenReady().then(createWindow);
+
+app.on("window-all-closed", function () {
+  if (process.platform !== "darwin") app.quit();
+});
